fe32b560a9dd7e600c9529657f5d2d25476b747e
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-12 02:24:39 +0900

    chapter 4

--- 4/4-18.cpp
+++ 4/4-18.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include <complex>
 
 int main()
 {
 	double a = 0, b = 0, c = 0;
-	double x1 = 0, x2 = 0;
 
 	std::cin >> a >> b >> c;
-	x1 = ((-b) + std::sqrt(std::pow(b, 2) - 4 * a * c)) / 2 * a;
-	x2 = ((-b) - std::sqrt(std::pow(b, 2) - 4 * a * c)) / 2 * a;
+	std::complex<double> x1 = ((-b) + std::sqrt(std::complex<double>((b * b - 4 * a * c), 0))) / (2 * a);
+	std::complex<double> x2 = ((-b) - std::sqrt(std::complex<double>((b * b - 4 * a * c), 0))) / (2 * a);
 
 	std::cout << x1 << " " << x2;
 }
