797d258bf311dc14c73b71f9879e1cd597854b61
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-11 17:34:57 +0900

    chapter 4

---
+++ 4/4-13.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+bool is_prime(int i)
+{
+	if (i == 1)
+		return false;
+
+	else if (i == 2)
+		return true;
+
+	for (auto j = 2; j * j <= i; ++j)
+		if (i % j == 0)
+			return false;
+
+	return true;
+}
+
+int main()
+{
+	std::vector<int> primes;
+	for (auto i = 1; i <= 100; ++i)
+	{
+		if (is_prime(i))
+			primes.push_back(i);
+	}
+	
+	for (auto x : primes)
+		std::cout << x << " ";
+}
