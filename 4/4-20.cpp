ab672f2a33b5ac02fa84b28f24d7b12146f05cae
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-12 02:55:13 +0900

    chapter 4

---
+++ 4/4-20.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+
+int main()
+{
+	std::vector<std::string> names;
+	std::vector<int> numbers;
+
+	std::string name;
+	int num = 0;
+
+	while (true)
+	{
+		std::cin >> name >> num;
+		if (name == "quit" && num == 0)
+			break;
+		names.push_back(name);
+		numbers.push_back(num);
+	}
+
+	std::vector<std::string> temp = names;
+	std::sort(temp.begin(), temp.end());
+	auto last = std::unique(temp.begin(), temp.end());
+	temp.erase(last, temp.end());
+	if (temp.size() != names.size())
+	{
+		std::cerr << "error : duplicated name";
+		return EXIT_FAILURE;
+	}
+	/*
+	for (auto i = 0; i < names.size(); ++i)
+		std::cout << "name: " << names[i] << " " << "score: " << numbers[i] << '\n';
+	*/
+
+	std::cout << "input a name\n";
+
+	std::string gg;
+	std::cin >> gg;
+	auto p = std::find(names.begin(), names.end(), gg);
+	if (p != names.end())
+		std::cout << numbers[std::distance(names.begin(), p)];
+	else
+		std::cerr << "you can't find that name";
+}
