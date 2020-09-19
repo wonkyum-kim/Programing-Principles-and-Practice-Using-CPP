93d17b6fef0fd020a27d5a53982cb8ef6e986f3b
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-19 12:43:40 +0900

    chapter 4

--- 4/4-4.cpp
+++ 4/4-4.cpp
@@ -5,26 +5,24 @@ int main()
 	int left = 1;
 	int right = 101;
 	char ch;
-	while (left < right)
-	{
-		if (right - left == 1)
+	while (left < right) {
+		if (right - left == 1) {
 			break;
+		}
 		int mid = (left + right) / 2;
 		std::cout << "당신이 생각하는 숫자가 " << mid << "보다 작은가요?(y or n)\n";
-		while (std::cin >> ch)
-		{
-			if (ch == 'y')
-			{
+		while (std::cin >> ch) {
+			if (ch == 'y') {
 				right = mid;
 				break;
 			}
-			else if (ch == 'n')
-			{
+			else if (ch == 'n') {
 				left = mid;
 				break;
 			}
-			else
+			else {
 				std::cout << "다시 입력하세요\n";
+			}
 		}
 	}
 	std::cout << left;
